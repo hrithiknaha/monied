4f0bb0635d70e2c87046f8315c2217be8b074e21
Hrithik Naha <naha.hrithkk@gmail.com>
2023-09-26 00:05:35 +0530

    made category as not required for repayment expenses

--- models/Expenses.js
+++ models/Expenses.js
@@ -5,7 +5,7 @@ const expenseSchema = new mongoose.Schema(
         name: { type: String, required: true },
         amount: { type: Number, required: true },
         transaction_date: { type: Date, required: true },
-        category: { type: String, required: true },
+        category: { type: String },
     },
     { timestamps: true }
 );
